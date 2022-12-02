1b3eb006eb4bead29205e74f0733604745d024ee
Simona Zozuľaková <szozulakova@gmail.com>
2022-12-02 20:16:20 +0100

    trying styled components

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
+const AddButton = styled.button`
+  background-color: #4e199d;
+`;
+
 const AddTask = ({ addTaskFn, filterFn }) => {
   const [value, setValue] = useState("");
   return (
     <div>
       <input onChange={(e) => setValue(e.target.value)} type="text" />
       <button
+        className={styles.hello}
         onClick={() => {
           addTaskFn(value);
         }}
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,7 @@
 const Task = ({ task, deleteFn, doneFn }) => {
   return (
-    <div>
-      <div>{task.title}</div>
+    <div className="taskWrapper">
+      <div className="task">{task.title}</div>
       <button onClick={() => doneFn(task.title)}>Done</button>
       <a href="#" onClick={() => deleteFn(task.title)}>
         Remove

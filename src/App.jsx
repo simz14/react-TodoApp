bb06dc8276ff5165e3f323b5db21b9b84cf728e6
Simona Zozuľaková <szozulakova@gmail.com>
2022-12-02 15:18:01 +0100

    creating data for tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  const [data, setData] = useState([
+    { title: "Task 1", done: false },
+    { title: "Task 2", done: false },
+    { title: "Task 3", done: false },
+    { title: "Task 4", done: false },
+    { title: "Task 5", done: false },
+  ]);
+
+  return (
+    <div>
+      <h1>ToDo list</h1>
+      <div>
+        {data.map((task) => (
+          <div key={task.title}>{task.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
